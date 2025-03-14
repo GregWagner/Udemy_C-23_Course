b5fdc288475038821f4b023c7fdaeb9efb2c324d
GregWagner <theGreg101@gmail.com>
2025-03-14 18:11:18 -0500

    Update

--- 02.EnvironmentSetup/01.Project_template_modules/main.cpp
+++ 02.EnvironmentSetup/01.Project_template_modules/main.cpp
@@ -1,16 +1,9 @@
-/*
-    . Project description
-        . Topic #1
-        . Topic #2
-*/
-
 #include <fmt/format.h>
 import utilities;
 
-int main(){
+auto main() -> int {
     fmt::print("Hello, World!\n");
     fmt::print("One\n");
     fmt::print("Two\n");
     fmt::print("Three\n");
-    return 0;
 }
