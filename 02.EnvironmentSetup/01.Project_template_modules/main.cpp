6bac90b95c49855c53bcbc3630f401d995736284
GregWagner <theGreg101@gmail.com>
2025-03-15 07:48:58 -0500

    Finished section 2

--- 02.EnvironmentSetup/01.Project_template_modules/main.cpp
+++ 02.EnvironmentSetup/01.Project_template_modules/main.cpp
@@ -2,8 +2,12 @@
 import utilities;
 
 auto main() -> int {
-    fmt::print("Hello, World!\n");
+    auto value1 {10};
+    auto value2{ 20 };
+
+    fmt::print("Hello, World! via print\n");
     fmt::print("One\n");
     fmt::print("Two\n");
     fmt::print("Three\n");
+    fmt::print("The values are {} and {}\n", value1, value2);
 }
